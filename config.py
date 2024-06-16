887af83e7b043486b2f0457a7b869cd3942933e6
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2024-06-16 23:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://aggressive-loise-popcornpanda-0868a8d6.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://aggressive-loise-popcornpanda-0868a8d6.koyeb.app/"
 
 
 
